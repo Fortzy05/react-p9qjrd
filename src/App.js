3a89c8bfc964424c1917983949a3631fc7eefec1
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-09-23 17:10:46 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <section id="intro">
+  <p className="name" >Hi, my name is <span >Omorodion Fortune.</span></p>
+  <h2>I develop, design the front end of websites, user experiences, user interfaces, digital experiences, etc.</h2>
+  <p>I'm a developer, a designer specializing in HTML, CSS, and JavaScript, user experience, your tech stack, etc.</p>
+  <p>Currently, <a href="https://github.com/fortzy05" target="_blank">Github</a>
+  </p> <p>I'm working at XYZ Corp, completing the XYZ Bootcamp, making a career switch from X to Y, etc.</p>
+</section>
+);
+
+}
