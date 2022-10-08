7d8fdf8d8bed6e88683ab7fa0ec3a4f2f1e5c670
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-10-08 13:29:36 +0100

    2 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import Contact from "../contact.js"
+import Contact from " ./contact.js"
 export default function App() {
   return (
     <section id="intro">
